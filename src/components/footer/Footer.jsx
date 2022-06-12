89405fb949faf1f1e806dc95f44a4b319926503b
chloesyy <seah11chloe@gmail.com>
2022-06-12 19:51:19 +0800

    remove sections

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,16 +13,16 @@ const Footer = () => {
             <li><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#experience">Experience</a></li>
-            <li><a href="#services">Services</a></li>
+            {/* <li><a href="#services">Services</a></li>
             <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#testimonials">Testimonials</a></li>
+            <li><a href="#testimonials">Testimonials</a></li> */}
             <li><a href="#contact">Contact</a></li>
         </ul>
 
         <div className="footer__socials">
-            <a href="https://facebook.com"><FaFacebookF/></a>
-            <a href="https://instagram.com"><FiInstagram/></a>
-            <a href="https://twitter.com"><IoLogoTwitter/></a>
+            <a href="https://www.facebook.com/chloee.syy/" target="_blank" rel="noreferrer"><FaFacebookF/></a>
+            <a href="https://www.instagram.com/chloe_syy_/" target="_blank" rel="noreferrer"><FiInstagram/></a>
+            <a href="https://twitter.com/chloe_syy_" target="_blank" rel="noreferrer"><IoLogoTwitter/></a>
         </div>
 
         <div className="footer__copyright">
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,12 +6,12 @@ const Services = () => {
   return (
     <section id='services'>
         <h5>What I Offer</h5>
-        <h2>Services</h2>
+        <h2>My Expertise</h2>
 
         <div className="container services__container">
             <article className='service'>
                 <div className='service__head'>
-                    <h3>UI/UX Design</h3>
+                    <h3>Data Engineering</h3>
                 </div>
 
                 <ul className='service__list'>
@@ -63,7 +63,7 @@ const Services = () => {
             </article>
             <article className='service'>
                 <div className='service__head'>
-                    <h3>Content Creation</h3>
+                    <h3>AI Engineering</h3>
                 </div>
 
                 <ul className='service__list'>
