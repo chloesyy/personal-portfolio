2f11ceea5a69199aa3ae2e9da0201aa77d93d220
chloesyy <seah11chloe@gmail.com>
2022-06-12 15:57:48 +0800

    complete tutorial

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {FaTelegramPlane} from 'react-icons/fa'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
+
+        <div className="container contact__container">
+            <div className="contact__options">
+                <article className="contact__option">
+                    <MdOutlineEmail className='contact__option-icon' />
+                    <h4>Email</h4>
+                    <h5>seah11chloe@gmail.com</h5>
+                    <a href="mailto:seah11chloe@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+                </article>
+                <article className="contact__option">
+                    <FaTelegramPlane className='contact__option-icon' />
+                    <h4>Telegram</h4>
+                    <h5>@chloe_syy</h5>
+                    <a href="https://t.me/chloe_syy" target="_blank" rel="noreferrer">Send a message</a>
+                </article>
+            </div>
+            {/* <form action="">
+                <input type="text"  name='name' placeholder='Your Full Name' required />
+                <input type="email" name='email' placeholder='Your Email' required />
+                <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+                <button type='submit' className='btn btn-primary'>Send Message</button>
+            </form> */}
+        </div>
+    </section>
   )
 }
 
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './experience.css'
-import {FiCheck} from 'react-icons/fi'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
@@ -13,35 +13,35 @@ const Experience = () => {
                 <h3>Frontend Development</h3>
                 <div className="experience__content">
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>HTML</h4>
                             <small className='text-light'>Intermediate</small>
                         </div>
                     </article>
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>CSS</h4>
                             <small className='text-light'>Intermediate</small>
                         </div>
                     </article>
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>Javascript</h4>
                             <small className='text-light'>Intermediate</small>
                         </div>
                     </article>
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>Bootstrap</h4>
                             <small className='text-light'>Intermediate</small>
                         </div>
                     </article>
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>React</h4>
                             <small className='text-light'>Intermediate</small>
@@ -53,28 +53,28 @@ const Experience = () => {
                 <h3>Backend Development</h3>
                 <div className="experience__content">
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>Node JS</h4>
                             <small className='text-light'>Intermediate</small>
                         </div>
                     </article>
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>MongoDB</h4>
                             <small className='text-light'>Basic</small>
                         </div>
                     </article>
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>PostgreSQL</h4>
                             <small className='text-light'>Intermediate</small>
                         </div>
                     </article>
                     <article className='experience__details'>
-                        <FiCheck className='experience__details-icon' />
+                        <BsPatchCheckFill className='experience__details-icon' />
                         <div>
                             <h4>Python</h4>
                             <small className='text-light'>Intermediate</small>
